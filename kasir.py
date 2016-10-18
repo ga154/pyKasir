01715a6bd6e6c81af0680aeae15a34e2f4d2a894
Gunawan Ariyanto <ga154@ums.ac.id>
2016-10-18 08:37:48 +0700

    tambah kode kasir.py

---
+++ kasir.py
@@ -0,0 +1,13 @@
+__author__ = 'Gunawan Ariyanto'
+
+from daftar_stok_barang import data_barang  # import data barang agar bisa dibaca di sini
+
+# inisialisasi data belanjaan yang akan dibeli dan diproses di lasir
+belanja = []
+item_belanja=[]
+
+# Masuk ke LOOP untuk memasukan data barang yang dibeli
+while True:
+    pass  # bagian ini harus dihapus apabila anda sudah mengerjakan soal2 di bawah ini
+    # 1. Tulislah pesan kepada kasir sbb: "Scan kode barang atau ketik q untuk keluar/selesai
+    # 2
