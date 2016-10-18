95ffc5026b7e8350329ed29e75676ee6a69f1fb3
Gunawan Ariyanto <ga154@ums.ac.id>
2016-10-18 08:23:57 +0700

    tambah file stok barang

---
+++ daftar_stok_barang.py
@@ -0,0 +1,9 @@
+__author__ = 'Gunawan Ariyanto'
+data_barang =   {
+                '111':['Es Krim Walls E', 4500],
+                '222':['Gelas Mug', 5000],
+                '333':['Sandal Jepit',7500],
+                '444':['Kopi Tora Bika Mocca Spesial Grande 700g ', 1500],
+                '555':['D500 Dispenser Air Sanken', 299900],
+		'666':['Spidol Snowman',6000],
+                }
